fa435b292d3ec0be392727c3020bcd9e3681a320
luongminhduong <duong40541@gmail.com>
2021-10-26 10:04:38 +0700

    first commit

---
+++ Vidu_4.14.js
@@ -0,0 +1,28 @@
+class Rectangle {
+    constructor (width = 5 , height = 10) {
+    this.__width = width;
+    this.height = height;
+    }
+    // Getter of property 'width'
+    get width() {
+    return this.__width;
+    }
+    // Setter of property 'width'
+    set width(newWidth) {
+    if(newWidth > 0) {
+    this.__width = newWidth;
+    } else {
+    console.log("Invalid width " + newWidth);
+    }
+    }
+    }
+    // ------------ TEST ------------------
+    var rect = new Rectangle(3, 5);
+    console.log("Height "+ rect.height); // Height: 5
+    console.log("Width "+ rect.width); // Width: 3
+    rect.width = -100;
+    console.log("Height "+ rect.height); // Height: 5
+    console.log("Width "+ rect.width); // Width: 3
+    rect.width = 100;
+    console.log("Height "+ rect.height); // Height: 5
+    console.log("Width "+ rect.width); // Width: 100
---
+++ Vidu_4.15.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor (name) {
+    // Private property: #name
+    this.#name = name;
+    }
+    // Getter of property name
+    get name() {
+    console.log("Call getter of property 'name'!!");
+    return this.#name;
+    }
+    }
